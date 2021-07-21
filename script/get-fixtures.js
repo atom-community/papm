0b47d681b19672f32a23870aac3f7f032aaa62cb
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-07-21 13:35:42 -0500

    test: use node-downloader-helper to download fixtures

--- script/get-fixtures.js
+++ script/get-fixtures.js
@@ -1,15 +1,18 @@
-const { execSync } = require("child_process")
+const { DownloaderHelper } = require("node-downloader-helper")
+const { join, dirname } = require("path")
 
-const execSettings = { shell: true, stdio: "inherit" }
-execSync(
-  `curl https://github.com/atom-community/apm/raw/master/spec/fixtures/node-v10.20.1.tar.gz -o "./spec/fixtures/node-v10.20.1.tar.gz"`,
-  execSettings
-)
-execSync(
-  `curl https://github.com/atom-community/apm/raw/master/spec/fixtures/node.lib -o "./spec/fixtures/node.lib"`,
-  execSettings
-)
-execSync(
-  `curl https://github.com/atom-community/apm/raw/master/spec/fixtures/node_x64.lib -o "./spec/fixtures/node_x64.lib"`,
-  execSettings
-)
+const downloadFolder = join(dirname(__dirname), "spec", "fixtures")
+
+const downloadOptions = {
+  override: { skip: true },
+}
+const links = [
+  "https://github.com/atom-community/apm/raw/master/spec/fixtures/node-v10.20.1.tar.gz",
+  "https://github.com/atom-community/apm/raw/master/spec/fixtures/node.lib",
+  "https://github.com/atom-community/apm/raw/master/spec/fixtures/node_x64.lib",
+]
+
+async function main() {
+  await Promise.all(links.map((link) => new DownloaderHelper(link, downloadFolder, downloadOptions).start()))
+}
+main()
